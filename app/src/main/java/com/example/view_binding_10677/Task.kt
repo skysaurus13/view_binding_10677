5945cbf5a66c16005558caa4c3a9ffba383f10fc
skysaurus13 <lukito2717@gmail.com>
2022-09-13 13:00:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10677/Task.kt
@@ -0,0 +1,4 @@
+package com.example.view_binding_10677
+
+data class Task(val chapter: String, val title: String, val details:String){
+}
---
+++ app/src/main/java/com/example/view_binding_10677/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10677
+
+object TaskList {
+    val taskList = listOf<Task> (
+        Task("Chapter 1", "Bangun Tidur", "Kebiasaan sehari-hari untuk membuat hari makin Indah"),
+        Task("Chapter 2", "Nguli Genshin Impact", "Mencari Primo buat Gacha"),
+        Task("Chapter 3", "Ngopi", "Mengapa Kopi Pahit?"),
+        Task("Chapter 4", "Gacha", "Hasil Gacha Ampas"),
+        Task("Chapter 5", "Upload ke Sosmed", "Tergarami")
+            )
+}
